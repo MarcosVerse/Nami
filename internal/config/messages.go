34fd9830e023d25a11a8e70dbe6df3eb9469cae6
Marcos Salles <marcowsgm@gmail.com>
2025-10-31 16:39:09 -0300

    [NEW] Add messages config
    Centralizes all response messages for users and login

---
+++ internal/config/messages.go
@@ -0,0 +1,20 @@
+package config
+
+const (
+
+    // usuário
+    UserCreated        = "Usuário criado com sucesso"
+    UserUpdated        = "Usuário atualizado com sucesso"
+    UserDeleted        = "Usuário deletado com sucesso"
+    UserNotFound       = "Usuário não encontrado"
+    InvalidUserID      = "ID de usuário inválido"
+
+    // login
+    InvalidCredentials = "Email ou senha incorretos"
+    TokenGenerationErr = "Erro ao gerar token"
+
+    // erros gerais
+    InternalServerError = "Erro interno do servidor"
+    BadRequest          = "Requisição inválida"
+	
+)
